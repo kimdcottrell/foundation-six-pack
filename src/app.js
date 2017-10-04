bbd08c5900f732d3494e3a967c20a83da1024cca
Kim Cottrell <kim@leftplusright.com>
2017-10-04 19:09:58 -0400

    webpack compiling dev and prod properly. code works in browsersync and away from it. now to setup src tree, make sourcemaps, test and hook into wordpress

--- src/app.js
+++ src/app.js
@@ -1,5 +1,5 @@
-const css = require('./app.scss'); //you need this to get the index html to auto generate the css
-console.log('hello from app.js');
+const css = require('./app.scss'); // you need this to get webpack to generate the css
+console.log('hello from app.js and working again');
 
 import $ from 'jquery';
 import whatInput from 'what-input';
@@ -9,6 +9,6 @@ window.$ = $;
 // import Foundation from 'foundation-sites';
 // If you want to pick and choose which modules to include, comment out the above and uncomment
 // the line below
-// import './foundation-pieces';
+import './foundation-pieces';
 
 $(document).foundation();
--- webpack.config.js
+++ webpack.config.js
@@ -1,61 +1,10 @@
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const ExtractTextPlugin = require("extract-text-webpack-plugin");
-const ManifestPlugin = require('webpack-manifest-plugin');
-const path = require('path');
-const webpack = require('webpack');
+const ExtractTextPlugin       = require("extract-text-webpack-plugin");
+const ManifestPlugin          = require('webpack-manifest-plugin');
+const BrowserSyncPlugin       = require('browser-sync-webpack-plugin');
+const CleanWebpackPlugin      = require('clean-webpack-plugin')
+const path                    = require('path');
 
 var isProd = process.env.NODE_ENV === 'production'; // true of false
-
-// setup dev vs prod css
-var cssDev = [
-  {
-    loader: "style-loader"
-  },
-  {
-    loader: "css-loader",
-    options: {
-      alias: {
-        // "../fonts/bootstrap": "bootstrap-sass/assets/fonts/bootstrap"
-      }
-    }
-  },
-  {
-    loader: "sass-loader",
-    options: {
-      includePaths: [
-        path.resolve('./node_modules/foundation-sites/scss'),
-        path.resolve('./node_modules/foundation-sites/scss/settings'),
-        path.resolve('./node_modules/motion-ui/src'),
-      ]
-    }
-  }
-];
-var cssProd = ExtractTextPlugin.extract({
-  fallback: 'style-loader',
-  //webpack reads loaders right to left
-  use: [
-    {
-      loader: "css-loader",
-      options: {
-        alias: {
-          // "../fonts/bootstrap": "bootstrap-sass/assets/fonts/bootstrap"
-        }
-      }
-    },
-    {
-      loader: "sass-loader",
-      options: {
-        includePaths: [
-          path.resolve('./node_modules/foundation-sites/scss'),
-          path.resolve('./node_modules/foundation-sites/scss/settings'),
-          path.resolve('./node_modules/motion-ui/src'),
-        ]
-      }
-    }
-  ],
-    publicPath: '/dist'
-  });
-var cssConfig = isProd ? cssProd : cssDev;
 
 module.exports = {
   entry: {
@@ -63,15 +12,39 @@ module.exports = {
     contact: './src/contact.js'
   },
   output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: '[name].[hash].js'
+    path: path.resolve(__dirname, 'assets'),
+    filename: 'js/[name].[hash].js'
     //use [chunkhash] for prod config, app.[hash].js for dev
   },
   module: {
     rules: [
       {
         test: /\.scss$/,
-        use: cssConfig
+        use: ExtractTextPlugin.extract({
+          fallback: 'style-loader',
+          //webpack reads loaders right to left
+          use: [
+              {
+                loader: "css-loader",
+                options: {
+                  alias: {
+                    // "../fonts/bootstrap": "bootstrap-sass/assets/fonts/bootstrap"
+                  }
+                }
+              },
+              {
+                loader: "sass-loader",
+                options: {
+                  includePaths: [
+                    path.resolve('./node_modules/foundation-sites/scss'),
+                    path.resolve('./node_modules/foundation-sites/scss/settings'),
+                    path.resolve('./node_modules/motion-ui/src'),
+                  ]
+                }
+              }
+            ],
+          publicPath: '/assets'
+        })
       },
       {
         test: /\.js$/,
@@ -85,32 +58,28 @@ module.exports = {
       }
     ]
   },
-  // all files in devServer are served from memory, so no files are created in /dist
-  devServer: {
-    contentBase: path.resolve(__dirname, 'dist'),
-    compress: true,
-    inline: true,
-    hot: true,
-    open: true,
-  },
   plugins: [
-    new HtmlWebpackPlugin({
-      title: 'Project',
-      filename: './../index.html',
-      template: './src/index.ejs'
-    }),
-    new HtmlWebpackPlugin({
-      title: 'Contact Page',
-      filename: './../contact.html',
-      template: './src/contact.ejs'
-    }),
     new ManifestPlugin(),
     new ExtractTextPlugin({
-      filename: 'app.[hash].css',
-      disable: !isProd,
+      filename: 'css/app.[hash].css',
       allChunks: true
     }),
-    new webpack.HotModuleReplacementPlugin(),
-    new webpack.NamedModulesPlugin()
+    new BrowserSyncPlugin( {
+        proxy: 'http://webpack.huzzah',
+        files: [
+            '**/*.php'
+        ],
+        ui: {
+          port: 8080
+        }
+      }
+    ),
+    new CleanWebpackPlugin(
+      './assets',
+      {
+        watch: true,
+        exclude: ['*.js'] // you need this or the .js randomly goes missing during watched builds
+      }
+    )
   ]
 }
